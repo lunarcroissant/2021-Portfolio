187000826a031ad4e87e44ef213c87684bbf80c3
lunarcroissant <76130184+lunarcroissant@users.noreply.github.com>
2021-11-17 15:01:51 +0000

    Filter display for skills added
    Added filter display for skills as well as a new colour

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
-var primaryColour = ["#303030", "#60D9FF", "#FF7171", "#FFE660", "#1F09A8", "#FFD37C", "#FF949A", "#F64B4B", "#4B7BF6", "#3B41D3", "#007B85"];
+var primaryColour = ["#303030", "#60D9FF", "#FF7171", "#FFE660", "#1F09A8", "#FFD37C", "#FF949A", "#F64B4B", "#4B7BF6", "#3B41D3", "#007B85", "#B4C4C6"];
 
-var secondaryColour = ["#F9F9F9", "#6A54F3", "#3B41D3", "#F35E54", "#8AFFDC", "#1F2531", "#3835DC", "#303030", "#E3CDFF", "#F9F9F9", "#CFFFF6"];
+var secondaryColour = ["#F9F9F9", "#6A54F3", "#3B41D3", "#F35E54", "#8AFFDC", "#1F2531", "#3835DC", "#303030", "#E3CDFF", "#F9F9F9", "#CFFFF6", "#424546"];
+
+// "linear-gradient(45deg, #7C4EFF, #3D73FF)"
 
 var colourSwapSpace = document.querySelector('.heroContent');
 
@@ -20,7 +22,7 @@ var a = Math.round(Math.random() * 6);
 colourSwapSpace.addEventListener("click", function () {
   // var k = i + 2;
 
-  var a = Math.round(Math.random() * 10);
+  var a = Math.round(Math.random() * 11);
 
   // alert(a);
 
@@ -162,3 +164,174 @@ colourInverter2.addEventListener("click", function colourSwitcher2() {
 //   console.log('why is it not doing anything???')
 // }
 
+// Skills filters
+
+var skills = [
+  {
+    skill: "React",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "JavaScript",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "GraphQL",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "HTML5",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "TypeScript",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "CSS3",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "Jest",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "jQuery",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "Git",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "BitBucket",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "GitLab",
+    status: "Experienced",
+    categories: ["All", "Code"]
+  },
+  {
+    skill: "Prototyping",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Figma",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Wireframing",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Interaction / UI design",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Journey mapping",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Photoshop",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Illustrator & Affinity Designer",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Adobe XD",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Spline",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Basic UX research",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "Content design",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  },
+  {
+    skill: "3D modelling",
+    status: "Experienced",
+    categories: ["All", "Design"]
+  }
+  
+];
+
+function displaySkills (filterTerm) {
+
+  console.log(filterTerm);
+
+  const insertionPoint = document.getElementById('skills-display');
+  const activeTab = document.getElementById(filterTerm);
+  const previousTab = document.getElementsByClassName('active')[0];
+
+  insertionPoint.innerHTML = '';
+  console.log('Cleared HTML');
+
+  while (insertionPoint.lastElementChild) {
+    insertionPoint.removeChild(insertionPoint.lastElementChild);
+    console.log(insertionPoint.lastElementChild);
+  }
+
+  previousTab.classList.remove('active');
+  activeTab.classList.add('active');
+
+  const getRelevantSkills = filterTerm === 'All' ? skills : skills.filter((item) => item.categories[1] == filterTerm);
+
+  // const getRelevantSkills = skills.filter((item) => item.categories[1] == filterTerm);
+  // e.target.classList.remove('active');
+
+  const skillTitles = getRelevantSkills.map(skillItem => skillItem.skill);
+
+  console.log(skillTitles);
+
+  console.log(insertionPoint);
+
+  const skillItems = (skillTitles.map(skill => {
+    var IndividualSkillItem = document.createElement('div');
+    var IndividualSkillItemText = document.createElement('p');
+    var InsertedTitle = document.createTextNode(skill);
+    IndividualSkillItem.classList.add('about__skillItem');
+    IndividualSkillItemText.classList.add('about__skillItem2');
+    IndividualSkillItemText.appendChild(InsertedTitle);
+    IndividualSkillItem.appendChild(IndividualSkillItemText);
+
+    document.getElementById("skills-display").appendChild(IndividualSkillItem);
+  }));
+};
+
+displaySkills('All');
+
+function alertClick () {
+  console.log("hi");
+}
+
+alertClick();
